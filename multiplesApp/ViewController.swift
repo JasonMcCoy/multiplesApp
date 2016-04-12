cd1dc5fb3ea4e44d0316dc842d63650d7d422e89
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-04-12 15:21:25 -0700

    Type in a number, Press the Add button, Watch your number add to itself
    This app will eventually take the user’s input (numerical) and then
    output a number and have it add to itself

---
+++ multiplesApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  multiplesApp
+//
+//  Created by Jason McCoy on 4/12/16.
+//  Copyright © 2016 Jason McCoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+
+}
+
