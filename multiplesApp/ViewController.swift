5bcf2668547cfd97cb84642d87ea61c24ac30e8d
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-04-17 02:10:50 -0700

    Need to Fix the ADD button
    1. If you press the Play button, nothing happens
    2. User is allowed to input a number into the text field
    3. If a user input a number into the text field and presses Play, then
    it’ll take them to the ADD screen
    4. Currently the ADD Screen will crash if pressed.
    
    Ultimately, pressing this button will take the current running sum, add
    the multiple to it and then give it a new sum

--- multiplesApp/ViewController.swift
+++ multiplesApp/ViewController.swift
@@ -10,8 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Properties
+    var maxTaps = 0
+    var currentTaps = 0
     
+    // First Screen user sees
+    @IBOutlet weak var multiplesLogo: UIImageView!
+    
+    @IBOutlet weak var inputMultipleTextField: UITextField!
 
+    @IBOutlet weak var playButton: UIButton!
 
+    // Second Screen user sees
+    @IBOutlet weak var pressAddToAddLabel: UILabel!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    // Press this and see the second screen
+    @IBAction func playButtonAction(sender: AnyObject) {
+        
+    if inputMultipleTextField.text != nil && inputMultipleTextField.text != "" {
+        
+        multiplesLogo.hidden = true
+        inputMultipleTextField.hidden = true
+        playButton.hidden = true
+        
+        pressAddToAddLabel.hidden = false
+        addButton.hidden = false
+            
+        updatePressToAddLabel()
+        }
+    }
+    
+        func restartGame() {
+            maxTaps = 0
+            inputMultipleTextField.text = ""
+            
+            multiplesLogo.hidden = false
+            inputMultipleTextField.hidden = false
+            playButton.hidden = false
+            
+            pressAddToAddLabel.hidden = true
+            addButton.hidden = true
+        }
+    
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+            } else {
+            return false
+        }
+    }
+    
+    func updatePressToAddLabel() {
+        inputMultipleTextField.text = "\(currentTaps) Taps"
+    }
+    
+    // Pressing this button will take the current running sum, add the multiple to it and then give it a new sum
+    @IBAction func addButtonAction(sender: AnyObject) {
+        currentTaps += 1
+        updatePressToAddLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
 }
 
